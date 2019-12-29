2a05bce2d6292138b1f725fcfab6dc54fea7e547
Hitesh Paliwal <pjhitesh007@gmail.com>
2019-12-29 19:25:09 +0530

    kyc

---
+++ artifacts/src/github.com/kyc_cc/go/kyc_cc.go
@@ -0,0 +1,163 @@
+
+ package main
+
+ /* Imports
+  * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
+  * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+  */
+ import (
+	 "bytes"
+	 "encoding/json"
+	 "fmt"
+	 "strconv"
+	 "io/ioutil"
+	 "os"
+ 
+	 "github.com/hyperledger/fabric/core/chaincode/shim"
+	 sc "github.com/hyperledger/fabric/protos/peer"
+ )
+ 
+ // Define the Smart Contract structure
+ type SmartContract struct {
+ }
+ 
+ type Users struct {
+    Users []User `json:"users"`
+}
+
+ // Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
+ type User struct {
+    Name   string `json:"name"`
+    Age    int    `json:"Age"`	
+}
+
+
+
+ /*
+  * The Init method is called when the Smart Contract "fabcar" is instantiated by the blockchain network
+  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
+  */
+ func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	 return shim.Success(nil)
+ }
+ 
+ /*
+  * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
+  * The calling application program has also specified the particular smart contract function to be called, with arguments
+  */
+ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+ 
+	 // Retrieve the requested Smart Contract function and arguments
+	 function, args := APIstub.GetFunctionAndParameters()
+	 // Route to the appropriate handler function to interact with the ledger appropriately
+	 if function == "queryKyc" {
+		 return s.queryKyc(APIstub, args)
+	 }else if function == "bulkupload" {
+		return s.bulkupload(APIstub, args)
+	} else if function == "queryAllKyc" {
+	    return s.queryAllKyc(APIstub, args)	
+	}
+ 
+     return shim.Error("Invalid Smart Contract function name.")
+ }
+ 
+
+func (s *SmartContract) queryKyc(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+ 
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	carAsBytes, _ := APIstub.GetState(args[0])
+	return shim.Success(carAsBytes)
+}
+
+ 
+
+ func (s *SmartContract) bulkupload(APIstub shim.ChaincodeStubInterface) sc.Response{
+ 
+	// Open our jsonFile
+    jsonFile, err := os.Open("./util/utildata.json")
+    // if we os.Open returns an error then handle it
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("Successfully Opened users.json")
+    // defer the closing of our jsonFile so that we can parse it later on
+    defer jsonFile.Close()
+
+    // read our opened xmlFile as a byte array.
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    // we initialize our Users array
+	var users Users
+    json.Unmarshal(byteValue, &users)
+
+  
+	for i := 0; i < len(users.Users); i++ {
+        
+		 kycAsBytes, _ := json.Marshal(users.Users[i])
+		 APIstub.PutState("Customer"+strconv.Itoa(i), kycAsBytes)
+		 fmt.Println("Added", users.Users[i])
+		 
+	 }
+ 
+	 return shim.Success(nil)
+	
+}
+
+ func (s *SmartContract) queryAllKyc(APIstub shim.ChaincodeStubInterface) sc.Response {
+ 
+	startKey := "Customer0"
+	endKey := "Customer999"
+
+	resultsIterator, err := APIstub.GetStateByRange(startKey, endKey)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- queryAllkyc:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+}
+
+
+
+ // The main function is only relevant in unit test mode. Only included here for completeness.
+ func main() {
+ 
+	 // Create a new Smart Contract
+	 err := shim.Start(new(SmartContract))
+	 if err != nil {
+		 fmt.Printf("Error creating new Smart Contract: %s", err)
+	 }
+ }
