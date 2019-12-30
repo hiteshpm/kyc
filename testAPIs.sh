0f88cd86a3086d9c1a05eb1238116373e8f7197c
Hitesh Paliwal <pjhitesh007@gmail.com>
2019-12-30 11:54:07 +0530

    Update testAPIs.sh

--- testAPIs.sh
+++ testAPIs.sh
@@ -57,7 +57,7 @@ echo
 ORG1_TOKEN=$(curl -s -X POST \
   http://localhost:4200/users \
   -H "content-type: application/x-www-form-urlencoded" \
-  -d 'username=Jim&orgName=Org1')
+  -d 'username=hitesh&orgName=Org1')
 echo $ORG1_TOKEN
 ORG1_TOKEN=$(echo $ORG1_TOKEN | jq ".token" | sed "s/\"//g")
 echo
@@ -68,7 +68,7 @@ echo
 ORG2_TOKEN=$(curl -s -X POST \
   http://localhost:4200/users \
   -H "content-type: application/x-www-form-urlencoded" \
-  -d 'username=Barry&orgName=Org2')
+  -d 'username=andy&orgName=Org2')
 echo $ORG2_TOKEN
 ORG2_TOKEN=$(echo $ORG2_TOKEN | jq ".token" | sed "s/\"//g")
 echo
